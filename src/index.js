e5a0f3e4ee1621a15935dba54eed29822ea35b0f
Quy Vu <tranquy2512@gmail.com>
2017-07-31 23:56:23 +0700

    config router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "react-router-redux";
 import store, { history } from "./state/store/StoreConfig";
 import injectTapEventPlugin from "react-tap-event-plugin";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { Route, Router, IndexRedirect } from "react-router";
 
 //Inject Tap event plugin for material ui
 injectTapEventPlugin();
@@ -17,13 +18,13 @@ const target = document.querySelector("#root");
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <div>
-        <MuiThemeProvider>
-          <App />
-        </MuiThemeProvider>
-      </div>
-    </ConnectedRouter>
+    <Router history={history}>
+      <MuiThemeProvider>
+        <Route>
+          <Route path="/" component={App} />
+        </Route>
+      </MuiThemeProvider>
+    </Router>
   </Provider>,
   target
 );
